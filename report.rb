cf55e63f7eecc08d28e43e94cdf3cc24fd02ff88
Pio Molina <vpio@comcast.net>
2018-09-20 12:10:22 -0400

    added state feature

--- report.rb
+++ report.rb
@@ -1,4 +1,5 @@
 require "prawn"
+require 'pry'
 
 class Report
 
@@ -7,11 +8,18 @@ class Report
     end
 
     def run
-      Prawn::Document.generate("country_report.pdf") do |pdf|
-        pdf.text @country.name
-        pdf.move_down 20
-        pdf.text "Currency: #{@country.currency.name}"
+        Prawn::Document.generate("country_report.pdf") do |pdf|
+          pdf.text @country.name
+          pdf.move_down 20
+          pdf.text "Currency: #{@country.currency.name}"
+          if @country.states.any?
+            pdf.move_down 20
+            pdf.text "States:"
+            pdf.move_down 20
+            @country.states.each do |code, state|
+              pdf.text state["name"]
+            end
+          end
+        end
       end
     end
-
-end
