1226cf46af10ebab542153afce6b7d84baeeeb50
Pio Molina <vpio@comcast.net>
2018-09-20 11:21:04 -0400

    initial commit

---
+++ gemo.rb
@@ -0,0 +1,13 @@
+require 'countries'
+
+puts "What country do you want a report on?"
+
+country_name = gets.strip
+
+country = ISO3166::Country.find_country_by_name(country_name)
+
+if country
+  puts "The flag for #{country.name} is #{country.emoji_flag}"
+else
+  puts "Could not find a country called #{country_name}"
+end
